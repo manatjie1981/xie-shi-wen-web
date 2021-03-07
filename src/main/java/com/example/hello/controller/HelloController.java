e89e006173b266b972ac5ae1827c8376c66145c2
manat.jieranaiwongkul <manatjie19@gmail.com>
2021-03-07 14:04:22 +0700

    call rresful api

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,11 +1,32 @@
 package com.example.hello.controller;
 
+import com.example.hello.config.HttpClientConfig;
+import com.example.hello.config.RestTemplateConfig;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.RestTemplate;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { RestTemplateConfig.class, HttpClientConfig.class })
 @Controller
 public class HelloController {
+
+	@GetMapping("/hello2Employees")
+	public String gethello2Employees(Model model) {
+		final String uri = "https://hello-to-service.herokuapp.com/api/v1/employees";
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
+
+		model.addAttribute("hello",result);
+
+		return "hello";
+	}
 
 	@GetMapping("/hello")
 	public String getHello(Model model) {
