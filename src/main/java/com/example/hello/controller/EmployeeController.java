ec43cb0020dd36250cf480d48d9b39479a00df95
manat.jieranaiwongkul <manatjie19@gmail.com>
2021-03-07 15:01:22 +0700

    call rresful api

--- src/main/java/com/example/hello/controller/EmployeeController.java
+++ src/main/java/com/example/hello/controller/EmployeeController.java
@@ -3,31 +3,27 @@ package com.example.hello.controller;
 import com.example.hello.config.HttpClientConfig;
 import com.example.hello.config.RestTemplateConfig;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { RestTemplateConfig.class, HttpClientConfig.class })
 @Controller
-public class HelloController {
+public class EmployeeController {
 
-	@GetMapping("/hello")
-	public String getHello(Model model) {
-		model.addAttribute("hello","Hello Spring Boot User");
-		return "hello";
-	}
-	
-	@GetMapping("/")
-	public String welcome(Model model) {
-		model.addAttribute("hello","			Welcome to xie shi web web			");
-		model.addAttribute("hello2","			 							First App on cloud			");
+	@GetMapping("/hello2Employees")
+	public String gethello2Employees(Model model) {
+		final String uri = "https://hello-to-service.herokuapp.com/api/v1/employees";
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
 
-		return "hello";
+		model.addAttribute("employeeList",result);
+
+		return "employee";
 	}
+
 }
