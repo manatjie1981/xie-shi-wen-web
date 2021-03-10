78f9529f51cefc33af6d3880d81e2ecf4b69e706
Manat Jieranaiwongkul-APNS (Thailand) <ManatJ-APNS@azay.co.th>
2021-03-10 14:32:36 +0700

    login page

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+	@GetMapping("/login")
+	public String getlogin(Model model) {
+		return "login";
+	}
+	
 	@GetMapping("/hello")
 	public String getHello(Model model) {
 		model.addAttribute("hello","Hello Spring Boot User");
