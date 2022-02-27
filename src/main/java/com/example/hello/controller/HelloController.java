783cffd7eedb6f4862d66e264460eaf2d4c7541f
manat <xxx@yyy.com>
2022-02-27 10:39:44 +0700

    check  permisiom

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -88,7 +88,7 @@ public class HelloController {
 //setting the driver executable
 		//System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver", dir+"//src//main//java//com//example//hello//test//chromedriver2.exe");
-		System.setProperty("webdriver.chrome.driver", dir+"//src//main//java//com//example//hello//test//chromedriver_linux64");
+		System.setProperty("webdriver.chrome.driver", dir+"//src//main//java//com//example//hello//chromedriver");
 
 		System.out.println("xxxxxxxxxx 2");
 //Initiating your chromedriver
